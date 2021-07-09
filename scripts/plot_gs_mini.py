62997b49c8829a443f85b947f9d5d7deb65d4563
Enrico Rinaldi <erinaldi.work@gmail.com>
2021-07-09 14:36:25 +0900

    [fix] update mini plot gs

--- scripts/plot_gs_mini.py
+++ scripts/plot_gs_mini.py
@@ -11,7 +11,7 @@ plt.style.use('./paper.mplstyle')
 l = "2.0"
 ls = l.replace(".", "")
 print(f"l{ls}_mini_gs.csv")
-data = pd.read_csv(f"l{ls}_mini_gs.csv", header=0, dtype={"Lambda": int})
+data = pd.read_csv(f"../data/l{ls}_mini_gs.csv", header=0, dtype={"Lambda": int})
 # %%
 fig, ax = plt.subplots()
 data[::2].plot(x="Lambda", y="Energy0", marker="o", label=rf"$\lambda$={l} (O)", ax=ax)
@@ -23,8 +23,8 @@ ax.grid()
 ax.set_ylabel(r"$E_0$", rotation=90)
 ax.set_xlabel(r"$\Lambda$")
 ax.legend(loc="lower right")
-plt.savefig(f"l{ls}_mini_gs.png")
-plt.savefig(f"l{ls}_mini_gs.pdf")
+plt.savefig(f"../figures/l{ls}_mini_gs.png")
+plt.savefig(f"../figures/l{ls}_mini_gs.pdf")
 # %%
 fig, ax = plt.subplots()
 data.diff()[::2].abs().plot(
@@ -37,8 +37,8 @@ ax.set_ylabel(r"$E_0^{diff}$", rotation=90)
 ax.set_xlabel(r"$\Lambda-3$")
 ax.grid()
 ax.legend(loc="upper right")
-plt.savefig(f"l{ls}_mini_gs_diff.png")
-plt.savefig(f"l{ls}_mini_gs_diff.pdf")
+plt.savefig(f"../figures/l{ls}_mini_gs_diff.png")
+plt.savefig(f"../figures/l{ls}_mini_gs_diff.pdf")
 
 # %%
 # for this ground state we know the energy should be zero
@@ -53,6 +53,6 @@ ax.set_ylabel(r"$E_0$", rotation=90)
 ax.set_xlabel(r"$\Lambda$")
 ax.grid()
 ax.legend(loc="upper right")
-plt.savefig(f"l{ls}_mini_gs_log.png")
-plt.savefig(f"l{ls}_mini_gs_log.pdf")
+plt.savefig(f"../figures/l{ls}_mini_gs_log.png")
+plt.savefig(f"../figures/l{ls}_mini_gs_log.pdf")
 # %%
